bf588dc006863f7fec5dffaf0c5e083f2f3781ce
Baishakhi <rayb@utexas.edu>
2014-12-12 17:44:18 -0800

    lot of structural change

---
+++ styles/toggle.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $("pl1").hide();
+  $("pl2").hide();
+  $("#lang_bib").click(function(){$("pl1").slideToggle(1000);});
+  $("#lang_sum").click(function(){$("pl2").slideToggle(1000);});
+
+  $("pl3").hide();
+  $("pl4").hide();
+  $("#frank_bib").click(function(){$("pl3").slideToggle(1000);});
+  $("#frank_sum").click(function(){$("pl4").slideToggle(1000);});
+
+  $("pl5").hide();
+  $("pl6").hide();
+  $("#spa_bib").click(function(){$("pl5").slideToggle(1000);});
+  $("#spa_sum").click(function(){$("pl6").slideToggle(1000);});
+
+  $("pl7").hide();
+  $("pl8").hide();
+  $("#api_bib").click(function(){$("pl7").slideToggle(1000);});
+  $("#api_sum").click(function(){$("pl8").slideToggle(1000);});
+
+  $("pl9").hide();
+  $("#thesis_bib").click(function(){$("pl9").slideToggle(1000);});
+
+  $("pl10").hide();
+  $("pl11").hide();
+  $("#bsd_bib").click(function(){$("pl10").slideToggle(1000);});
+  $("#bsd_sum").click(function(){$("pl11").slideToggle(1000);});
+
+  $("pl12").hide();
+  $("pl13").hide();
+  $("#rep_bib").click(function(){$("pl12").slideToggle(1000);});
+  $("#rep_sum").click(function(){$("pl13").slideToggle(1000);});
+
+  $("pl14").hide();
+  $("pl15").hide();
+  $("#sup_bib").click(function(){$("pl14").slideToggle(1000);});
+  $("#sup_sum").click(function(){$("pl15").slideToggle(1000);});
+
+  $("pl16").hide();
+  $("pl17").hide();
+  $("#gpu_bib").click(function(){$("pl16").slideToggle(1000);});
+  $("#gpu_sum").click(function(){$("pl17").slideToggle(1000);});
+
+});
