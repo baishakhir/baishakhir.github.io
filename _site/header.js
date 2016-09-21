9e6d1aea210a6551e28ed84e4237f40ca51aeade
Baishakhi Ray <Baishakhi Ray>
2016-09-20 21:30:46 -0400

    trying to get rid off jkyll error

---
+++ _site/header.js
@@ -0,0 +1,35 @@
+﻿document.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\
+<meta property=\"og:type\" content=\"article\">\
+<meta property=\"og:image\" content=\"./images/shoot.jpg\">\
+<meta property=\"og:site_name\" content=\"rayb\">\
+<meta property=\"og:description\" content=\"Check out http://rayb.info!  \">\
+<style type=\"text/css\">body { margin:0; padding:0; }</style>\
+\
+<script src=\"./jquery.min.js\"></script>  \
+<script src=\"styles/toggle.js\"></script>\
+\
+<link rel=\"stylesheet\" type=\"text/css\" id=\"toggle.css\" href=\"styles/toggle.css\">\
+\
+<script>\
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\
+\
+  ga('create', 'UA-58677335-1', 'auto');\
+  ga('send', 'pageview');\
+\
+</script>\
+\
+\
+<script type=\"text/javascript\" src=\"./styles/imageSwap.js\"></script>\
+<script type=\"text/javascript\">		\
+		MM_preloadImages('images/shoot.jpg')\
+</script>\
+\
+\
+<link rel=\"stylesheet\" type=\"text/css\" id=\"layoutcontainer.css\" href=\"./styles/layoutcontainer.css\">\
+<link rel=\"stylesheet\" type=\"text/css\" id=\"theme.css\" href=\"./styles/theme.css\">\
+<link rel=\"stylesheet\" type=\"text/css\" id=\"color_1.css\" href=\"./styles/color_1.css\">\
+    \
+<style type=\"text/css\"></style>");
